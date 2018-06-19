010e141d48eeb36fe8d718ee727da9e2425f642b
zakiasegond <segondzakia@live.fr>
2018-06-19 14:45:21 +0200

    num de 4 à 9 fini

--- cookie.js
+++ cookie.js
@@ -1,13 +1,32 @@
-console.log ('hello');
+
 var div = $('#display');
 var press = $('#click');
 var score = 0;
-
+var multiplier = 1;
+var augmenter=$('#multiply');
+var prix = 50;
 
 	press.click (function()
 	{
-		score++;
+		score = score+multiplier;
 		div.html(score);
+	});
+
+
+	augmenter.click (function ()
+	{
+		multiplier ++;
+		score = score-prix;
+
+		if (score<0)
+		{
+			score=0;
+		}
+
+		div.html(score);
+		augmenter.html("Multiplicateur x " + multiplier);
+		prix = prix * 2;
+
 	});
 
 
